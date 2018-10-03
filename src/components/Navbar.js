6b60ec296a852cc1b5efdf20dea4df558dbdb3c7
Fernando Pulsa <fernandopulsa@gmail.com>
2018-10-03 20:43:21 +0200

    FRONT/ First Changes on the nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
+        La Fiambrera  
+         {/* <figure className="image">
             <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+          </figure> */}
         </Link>
       </div>
       <div className="navbar-start">
@@ -20,18 +19,6 @@ const Navbar = () => (
         <Link className="navbar-item" to="/products">
           Products
         </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
       </div>
     </div>
   </nav>
