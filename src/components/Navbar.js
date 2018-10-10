be4ba88981690c065efc5bcf4fba2335ea8e69a7
Fernando Pulsa <fernandopulsa@gmail.com>
2018-10-10 16:31:58 +0200

    FRONT/ Cover img reduced and links added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,10 +16,28 @@ const Navbar = () => (
       </div>
       <div className="RRSS_wrapper">
         <p className="RRSS_item color_corpo">
-          <a href="https://twitter.com/fiambreraxyz">Twitter</a>
+          <a target="blank" href="https://www.spreaker.com/show/3170279/episodes/feed">RSS Feed</a>
         </p>
         <p className="RRSS_item color_corpo">
-          <a href="https://www.instagram.com/fiambreraxyz/">Instagram</a>
+          <a target="blank" href="https://open.spotify.com/show/3NPtLDGBNOdrZsrSbreyI2?si=PPBUtmCfTQW3KIJfeSUxTg">Spotify</a>
+        </p>
+        <p className="RRSS_item color_corpo">
+          <a target="blank" href="https://itunes.apple.com/es/podcast/la-fiambrera/id1437938605?mt=2">iTunes</a>
+        </p>
+        <p className="RRSS_item color_corpo">
+          <a target="blank" href="https://www.ivoox.com/podcast-fiambrera_sq_f1621292_1.html">iVoox</a>
+        </p>
+        <p className="RRSS_item color_corpo">
+          <a target="blank" href="https://pca.st/rd5q">Pocket Casts</a>
+        </p>
+        <p className="RRSS_item color_corpo">
+          <a target="blank" href="https://overcast.fm/itunes1437938605/la-fiambrera">Overcast</a>
+        </p>
+        <p className="RRSS_item color_corpo">
+          <a target="blank" href="https://twitter.com/fiambreraxyz">Twitter</a>
+        </p>
+        <p className="RRSS_item color_corpo">
+          <a target="blank" href="https://www.instagram.com/fiambreraxyz/">Instagram</a>
         </p>
         {/* <Link className="navbar-item" to="/about">
           About
