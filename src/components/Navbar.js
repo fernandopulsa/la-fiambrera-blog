d7d60f8e95b73dd35ba55cea0a3d8651877f82ff
Fernando Pulsa <fernandopulsa@gmail.com>
2018-10-17 23:14:51 +0200

    Nav RRSS added icon + some style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'gatsby'
 // import logo from '../img/fiambrera-logo.png'
 import caratula from '../img/caratula.png'
+import twitter from '../img/twitter.svg'
+import insta from '../img/instagram-logo.svg'
 
 const Navbar = () => (
   <nav>
@@ -9,38 +11,46 @@ const Navbar = () => (
         {/* <Link to="/" className="title">
             <img src={logo} alt="logo" className="logo-img" />
         </Link> */}
-
-        <Link to="/" className="title">
-            <img src={caratula} alt="logo" className="logo-img" />
-        </Link>
+        <div className="wrapper">
+          <Link to="/" className="title">
+              <img src={caratula} alt="logo" className="logo-img" />
+          </Link>
+        </div>
+        <div className="wrapper">
+         <p>¡Un pdodcast to gupao</p>
+        </div>
       </div>
       <div className="RRSS_wrapper">
-        <p className="RRSS_item color_corpo">
+        <p className="listen_item color_corpo">
           <a target="blank" href="https://www.spreaker.com/show/3170279/episodes/feed">RSS Feed</a>
         </p>
-        <p className="RRSS_item color_corpo">
+        <p className="listen_item color_corpo">
           <a target="blank" href="https://open.spotify.com/show/3NPtLDGBNOdrZsrSbreyI2?si=PPBUtmCfTQW3KIJfeSUxTg">Spotify</a>
         </p>
-        <p className="RRSS_item color_corpo">
+        <p className="listen_item color_corpo">
           <a target="blank" href="https://itunes.apple.com/es/podcast/la-fiambrera/id1437938605?mt=2">iTunes</a>
         </p>
-        <p className="RRSS_item color_corpo">
+        <p className="listen_item color_corpo">
           <a target="blank" href="https://www.ivoox.com/podcast-fiambrera_sq_f1621292_1.html">iVoox</a>
         </p>
-        <p className="RRSS_item color_corpo">
+        <p className="listen_item color_corpo">
           <a target="blank" href="https://pca.st/rd5q">Pocket Casts</a>
         </p>
-        <p className="RRSS_item color_corpo">
+        <p className="listen_item color_corpo">
           <a target="blank" href="https://overcast.fm/itunes1437938605/la-fiambrera">Overcast</a>
         </p>
-        <p className="RRSS_item color_corpo">
+        <p className="divider color_corpo">
             |
         </p>
-        <p className="RRSS_item color_corpo">
-          <a target="blank" href="https://twitter.com/fiambreraxyz">Twitter</a>
+        <p className="RRSS_item_wrapper color_corpo">
+          <a target="blank" href="https://twitter.com/fiambreraxyz">
+            <img src={twitter} alt="logo" className="rrss_img" />
+          </a>
         </p>
-        <p className="RRSS_item color_corpo">
-          <a target="blank" href="https://www.instagram.com/fiambreraxyz/">Instagram</a>
+        <p className="RRSS_item_wrapper color_corpo">
+          <a target="blank" href="https://www.instagram.com/fiambreraxyz/">
+           <img src={insta} alt="logo" className="rrss_img" />
+          </a>
         </p>
         {/* <Link className="navbar-item" to="/about">
           About
